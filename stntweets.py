641548fe286ccedcca4176664f9325c849963422
Parker Higgins <parker@parkerhiggins.net>
2017-02-04 13:05:20 -0800

    Broke comparing and tweeting out into separate functions

--- stntweets.py
+++ stntweets.py
@@ -33,9 +33,71 @@ def available_over_https(site):
     else:
         return False
 
+def compare_results(old_site, new_site):
+
+    site_tweets = []
+
+    if old_site == None:
+         site_tweets.append("🤖 We're tracking a new site on @SecureTheNews: " + new_site['name'] + " has a grade of " + new_site['grade'] + ". " + new_site['url'])
+         return site_tweets
+
+    if (new_site['grade'] != old_site['grade'] and 
+        new_site['score'] > old_site['score']):
+
+         site_tweets.append("🤖 " + new_site['name'] + " has improved its grade on the @SecureTheNews leaderboard from " + old_site['grade'] + " to " + new_site['grade'] + ". " + new_site['url'])
+
+    if (available_over_https(new_site)
+        and not available_over_https(old_site)):
+
+        site_tweets.append("🤖 " + new_site['name'] + " is now available over HTTPS! Next step: turn it on by default. https://securethe.news/sites")
+
+    if (new_site['defaults_to_https']
+        and not old_site['defaults_to_https']):
+
+        site_tweets.append("🤖 Great news: " + new_site['name'] + " is now using HTTPS by default! Huge win for reader privacy and security. https://securethe.news/sites")
+
+    if (new_site['hsts'] and not old_site['hsts']):
+
+        site_tweets.append("🤖 " + new_site['name'] + " is now using HSTS headers. This means browsers will connect to it more securely. Yes! https://securethe.news/sites")
+
+    if (new_site['hsts_preloaded'] 
+        and not old_site['hsts_preloaded']):
+
+        site_tweets.append("🤖 " + new_site['name'] + " is now on the HSTS preload list for major browsers, protecting user privacy. Bravo. https://securethe.news/sites")
+
+    return site_tweets
+
+def tweet_results(tweets):
+    to_sleep = False
+
+    for site in tweets:
+
+        if to_sleep:
+            time.sleep(1800)
+        else:
+            to_sleep = True
+
+        reply_to = 'null'
+
+        for item in site:
+
+            if reply_to != 'null':
+                time.sleep(30)
+
+            try:
+                response = twitter.update_status(status=item,
+                    in_reply_to_status_id=reply_to)
+                reply_to = response['id_str']
+
+            except TwythonError as err:
+                twitter.send_direct_message(screen_name=botmaster,
+                    text="I tried to tweet " + item + 
+                    " but got the error " + str(err))
+
 def main():
     config = get_config()
     twitter = get_twitter_instance(config)
+    botmaster = config['botmaster']
 
     p = requests.get("https://securethe.news/sites")
     soup = BeautifulSoup(p.text, "html.parser")
@@ -52,7 +114,15 @@ def main():
     results = json.loads(script.text.strip('\n var STNsiteData =;'))
 
     for site in results:
-        new_results.append({'name':site['name'],'grade':site['grade']['grade'], 'score':site['score'], 'valid_https':site['valid_https'], 'downgrades_https':site['downgrades_https'], 'defaults_to_https':site['defaults_to_https'], 'hsts':site['hsts'], 'hsts_preloaded':site['hsts_preloaded']})
+        new_results.append({'name':site['name'],
+            'grade':site['grade']['grade'],
+            'score':site['score'],
+            'valid_https':site['valid_https'],
+            'downgrades_https':site['downgrades_https'],
+            'defaults_to_https':site['defaults_to_https'],
+            'hsts':site['hsts'],
+            'hsts_preloaded':site['hsts_preloaded'],
+            'url':'https://securethe.news' + site['absolute_url']})
 
 # Open existing results to compare to the new ones, if they exist.
 # If not, save the new ones and we'll compare later.
@@ -70,37 +140,8 @@ def main():
         for new_site in new_results:
 
             old_site = next((s for s in old_results if s['name'] == new_site['name']),None)
-            site_tweets = []
 
-            if old_site == None:
-
-                site_tweets.append("🤖 We're tracking a new site on @SecureTheNews: " + new_site['name'] + " has a grade of " + new_site['grade'] + ". https://securethe.news/sites/")
-                tweets.append(site_tweets)
-                continue
-
-            if (new_site['grade'] != old_site['grade'] and 
-                new_site['score'] > old_site['score']):
-
-                site_tweets.append("🤖 " + new_site['name'] + " has improved its grade on the @SecureTheNews leaderboard from " + old_site['grade'] + " to " + new_site['grade'] + ". https://securethe.news/sites/")
-
-            if (available_over_https(new_site)
-                and not available_over_https(old_site)):
-
-                site_tweets.append("🤖 " + new_site['name'] + " is now available over HTTPS! Next step: turn it on by default. https://securethe.news/sites")
-
-            if (new_site['defaults_to_https']
-                and not old_site['defaults_to_https']):
-
-                site_tweets.append("🤖 Great news: " + new_site['name'] + " is now using HTTPS by default! Huge win for reader privacy and security. https://securethe.news/sites")
-
-            if (new_site['hsts'] and not old_site['hsts']):
-
-              site_tweets.append("🤖 " + new_site['name'] + " is now using HSTS headers. This means browsers will connect to it more securely. Yes! https://securethe.news/sites")
-
-            if (new_site['hsts_preloaded']
-                and not old_site['hsts_preloaded']):
-
-                site_tweets.append("🤖 " + new_site['name'] + " is now on the HSTS preload list for major browsers, protecting user privacy. Bravo. https://securethe.news/sites")   
+            site_tweets = compare_results(old_site, new_site)
 
 # After all the comparisons, add the collected differences as a list
 # to the `tweets` list.
@@ -111,36 +152,14 @@ def main():
 # The tweeting loop.
 
         if len(tweets) == 0:
-            twitter.send_direct_message(screen_name='xor',
+            twitter.send_direct_message(screen_name=botmaster,
                 text="I would've sent a message, but nothing changed!")
 
-        to_sleep = False
+#        tweet_results(tweets)
 
-        for site in tweets:
+        print(tweets)
 
-            if to_sleep:
-                time.sleep(1800)
-            else:
-                to_sleep = True
-
-            reply_to = 'null'
-
-            for item in site:
-
-                if reply_to != 'null':
-                    time.sleep(30)
-
-                try:
-                    response = twitter.update_status(status=item,
-                        in_reply_to_status_id=reply_to)
-                    reply_to = response['id_str']
-                except TwythonError as err:
-                    twitter.send_direct_message(screen_name='xor',
-                    text="I tried to tweet " + item + 
-                    " but got the error " + str(err))
-
-
-        write_results(new_results, results_path)
+#        write_results(new_results, results_path)
 
     else:
         old_results = new_results
